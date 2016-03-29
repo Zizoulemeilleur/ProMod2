8cee9fe08f0f4d26358b417e227cdad1c7f12e03
SS <SS@ss-pro.hfu-open.fu.hs-furtwangen.de>
2016-03-29 21:06:09 +0200

    asd

--- ProMod2/src/team/sheldon/AdreliMain.java
+++ ProMod2/src/team/sheldon/AdreliMain.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,229 +17,410 @@ import java.util.Scanner;
  */
 public class AdreliMain {
 
-  /**
-   * Ist die Applikation noch am laufen, oder soll beendet werden?
-   */
-  private boolean isRunning = true;
-  /**
-   * Die in-memory gepufferted Personen
-   */
-  private ArrayList<Person> personen = new ArrayList<>();
-  /**
-   * Ein Scanner, der in der ganzen applikation benutzt wird.
-   */
-  private final Scanner scanner;
+	/**
+	 * Ist die Applikation noch am laufen, oder soll beendet werden?
+	 */
+	private boolean isRunning = true;
+	/**
+	 * Die in-memory gepufferted Personen
+	 */
+	private ArrayList<Person> personen = new ArrayList<>();
+	/**
+	 * Ein Scanner, der in der ganzen applikation benutzt wird.
+	 */
+	private final Scanner scanner;
 
-  /**
-   * Constructor.
-   */
-  public AdreliMain() {
-    scanner = new Scanner(System.in);
-  }
+	/**
+	 * Constructor.
+	 */
+	public AdreliMain() {
+		scanner = new Scanner(System.in);
+	}
 
-  public static void main(String[] args) throws IOException {
-    // Applikation wird erstellt.
-    AdreliMain main = new AdreliMain();
+	public static void main(String[] args) throws IOException {
+		// Applikation wird erstellt.
+		AdreliMain main = new AdreliMain();
 
-    // und solange in einer endlosschleife bis das isRunning flag auf `false`
-    // gesetzt wird.
-    // TODO Exception Handling
-    while (main.isRunning()) {
-      // Pro Schleife wird immer als erstes das Menu ausgegeben
-      main.printMenu();
+		// und solange in einer endlosschleife bis das isRunning flag auf
+		// `false`
+		// gesetzt wird.
+		// TODO Exception Handling
+		while (main.isRunning()) {
+			// Pro Schleife wird immer als erstes das Menu ausgegeben
+			main.printMenu();
+			try {
 
-      // und dann der User Input abgefragt
-      String nextInput = main.getScanner()
-          .next();
-      // TODO Exception Handling des parsens
-      int number = Integer.parseInt(nextInput);
+				// und dann der User Input abgefragt
+				String nextInput = main.getScanner().next();
+				// TODO Exception Handling des parsens
+				int number = Integer.parseInt(nextInput);
 
-      // und dann ausgewertet.
-      switch (number) {
-      case 0:
-        // `isRunning` wird auf false gesetzt, und bei der naechsten Schleife
-        // das Programm somit beendet.
-        main.setRunning(false);
-        System.out.println("Ciao");
-        break;
-      case 1:
-        // Person wird erfasst und dem Array hinzugefuegt
-        Person erfasstePerson = main.erfassePerson();
-        // TODO exception handling
-        if (erfasstePerson != null) {
-          main.addPerson(erfasstePerson);
-        }
-        break;
-      case 2:
-        // Alle Personen werden angezeigt
-        main.displayPersons();
-        break;
-      case 3:
-        System.out.print("Dateiname zum speichern eingeben: ");
-        // TODO Exception handling
-        String filenameForSave = main.getScanner()
-            .next();
-        main.savePersons(filenameForSave);
-        break;
-      case 4:
-        System.out.println("Dateiname zum laden eingeben: ");
-        // TODO exception handling
-        main.loadPersons(main.getScanner()
-            .next());
-        break;
-      case 5:
-        System.out.println("Dateiname zum loeschen eingeben: ");
-        // TODO Exception handling
-        main.deleteFile(main.getScanner()
-            .next());
-        break;
-      default:
-        // Wenn der Benutzerinput nicht passt, dann wird nichts gemacht.
-        // TODO Fehler anzeigen
-      }
-    }
-  }
+				// und dann ausgewertet.
+				switch (number) {
+				case 0:
+					// `isRunning` wird auf false gesetzt, und bei der naechsten
+					// Schleife
+					// das Programm somit beendet.
+					main.setRunning(false);
+					System.out.println("Ciao");
+					break;
+				case 1:
+					// Person wird erfasst und dem Array hinzugefuegt
+					Person erfasstePerson = main.erfassePerson();
+					// TODO exception handling
+					if (erfasstePerson != null) {
+						main.addPerson(erfasstePerson);
+					}
+					break;
+				case 2:
+					// Alle Personen werden angezeigt
+					main.displayPersons();
+					break;
+				case 3:
+					System.out.print("Dateiname zum speichern eingeben: ");
+					// TODO Exception handling
+					String filenameForSave = main.getScanner().next();
+					main.savePersons(filenameForSave);
+					break;
+				case 4:
+					System.out.println("Dateiname zum laden eingeben: ");
+					// TODO exception handling
+					main.loadPersons(main.getScanner().next());
+					break;
+				case 5:
+					System.out.println("Dateiname zum loeschen eingeben: ");
+					// TODO Exception handling
+					main.deleteFile(main.getScanner().next());
+					break;
+				case 6:
+					System.out.println("in-memory Records sortieren");
+					main.sortiere();
+					break;
 
-  /**
-   * Delete the given file.
-   * 
-   * @param filename
-   *          the given filename/path
-   */
-  private void deleteFile(String filename) {
-    // TODO Exception Handling
-    File file = new File(filename);
-    if (file.delete()) {
-      System.out.println("Datei: " + filename + " wurde geloescht.");
-    } else {
-      System.out.println("Loeschen der Datei " + filename + " hat leider nicht geklappt.");
-    }
-  }
+				default:
+					// Wenn der Benutzerinput nicht passt, dann wird nichts
+					// gemacht.
+					// TODO Fehler anzeigen
+				}
+			} catch (Exception ex) {
+				System.out.println("Ups, da ist etwas schief gelaufen :-(");
+				System.out.println(ex.getMessage());
 
-  /**
-   * Displays all Persons.
-   */
-  private void displayPersons() {
-    for (Person person : personen) {
-      System.out.println(person);
-      try {
-        // Der Benutzer muss nach jeder gezeigten Person mit `ENTER`
-        // bestaetigen.
-        System.in.read();
-      } catch (IOException e) {
-      }
-    }
-  }
+			}
+		}
+	}
 
-  /**
-   * Add a Person to the `personen` Array.
-   * 
-   * @param person
-   *          the person to add
-   */
-  private void addPerson(Person person) {
-    personen.add(person);
-  }
+	/**
+	 * Delete the given file.
+	 * 
+	 * @param filename
+	 *            the given filename/path
+	 */
+	private void deleteFile(String filename) {
+		// TODO Exception Handling
+		File file = new File(filename);
+		if (file.delete()) {
+			System.out.println("Datei: " + filename + " wurde geloescht.");
+		} else {
+			System.out.println("Loeschen der Datei " + filename
+					+ " hat leider nicht geklappt.");
+		}
+	}
 
-  /**
-   * Print the commands menu to the console.
-   */
-  private void printMenu() {
-    System.out.println("ADRELI - Adressverwaltung");
-    System.out.println("Wollen Sie ...");
-    System.out.println("0. Programm Beenden");
-    System.out.println("1. Neue Person anlegen");
-    System.out.println("2. Alle Anzeigen");
-    System.out.println("3. Personen speichern");
-    System.out.println("4. Personen laden");
-    System.out.println("5. Personen Datei loeschen");
-    System.out.println();
-    System.out.println("Bitte tippen Sie ein Menupunkt und bestaetigen mit Enter [0-5]:");
-  }
+	/**
+	 * Displays all Persons.
+	 */
+	private void displayPersons() {
+		for (Person person : personen) {
+			System.out.println(person);
+			try {
+				// Der Benutzer muss nach jeder gezeigten Person mit `ENTER`
+				// bestaetigen.
+				System.in.read();
+			} catch (IOException e) {
+			}
+		}
+	}
 
-  /**
-   * Record all properties for a person and create a new person.
-   * 
-   * @return the newly created person.
-   */
-  private Person erfassePerson() {
-    Person neuePerson = null;
-    System.out.println("Geben Sie bitte die Daten ein:");
-    System.out.print("Name: ");
+	/**
+	 * Add a Person to the `personen` Array.
+	 * 
+	 * @param person
+	 *            the person to add
+	 */
+	private void addPerson(Person person) {
+		personen.add(person);
+	}
 
-    String name = getScanner().next();
-    System.out.print("Vorname: ");
-    String vorname = getScanner().next();
+	/**
+	 * Print the commands menu to the console.
+	 */
+	private void printMenu() {
+		System.out.println("ADRELI - Adressverwaltung");
+		System.out.println(" ");
+		System.out.println("Wollen Sie ...");
+		System.out.println("0. Programm Beenden");
+		System.out.println("1. Neue Person anlegen");
+		System.out.println("2. Alle Anzeigen");
+		System.out.println("3. Personen speichern");
+		System.out.println("4. Personen laden");
+		System.out.println("5. Personen Datei loeschen");
+		System.out.println("6. Personen sortieren");
+		System.out.println();
+		System.out
+				.println("Bitte tippen Sie ein Menupunkt und bestaetigen mit Enter [0-6]:");
+	}
 
-    // TODO Exception Handling
-    if (name != null && vorname != null) {
-      neuePerson = new Person(name, vorname);
-    }
+	/**
+	 * Record all properties for a person and create a new person.
+	 * 
+	 * @return the newly created person.
+	 */
+	private Person erfassePerson() {
+		Person neuePerson = null;
+		System.out.println("Geben Sie bitte die Daten ein:");
+		System.out.println(" ");
+		// Scanner wird aufgerufen, gibt der Variable name einen Wert des Types
+		// String .next() legt fest dass die Methode bis zur Eingabe wartet und
+		// nicht weitergeht.
+		System.out.print("Vorname: ");
+		String vorname = erfasseVorname();
+		System.out.print("Name: ");
+		String name = erfasseName();
+		System.out.print("Anrede: ");
+		String anrede = erfasseAnrede();
+		System.out.print("Straße: ");
+		String straße = erfasseStrasse();
+		System.out.print("PLZ: ");
+		String plz = erfassePLZ();
+		System.out.print("Ort: ");
+		String ort = erfasseOrt();
+		System.out.print("Telefon: ");
+		String telefon = erfasseTelefon();
+		System.out.print("Fax: ");
+		String fax = erfasseFax();
+		System.out.print("Bemerkung: ");
+		String bemerkung = erfasseBemerkung();
 
-    return neuePerson;
-  }
+		// Mehtode immer klammern, klein geschrieben, "NEW" ist ein reserviertes
+		// Keyword
+		// TODO Exception Handling
+		if (name != null && vorname != null && anrede != null && straße != null
+				&& plz != null && ort != null && telefon != null && fax != null
+				&& bemerkung != null) {
+			neuePerson = new Person(name, vorname, anrede, straße, plz, ort,
+					telefon, fax, bemerkung);
+		}
 
-  /**
-   * Saves the in-memory `personen` to the given file.
-   * 
-   * @param filename
-   *          the filename
-   * @throws IOException
-   */
-  public void savePersons(String filename) throws IOException {
-    List<String> lines = new ArrayList<>();
+		return neuePerson;
+	}
 
-    for (Person person : personen) {
-      // Create a serialized String of each person
-      lines.add(person.toSerializedString());
-    }
+	private String erfasseVorname() {
+		String vorname = null;
+		vorname = getScanner().next();
 
-    Path file = Paths.get(filename);
-    // TODO Zuerst file loeschen?
-    // TODO Exception Handling
-    // Write all serialized strings to the given file.
-    Files.write(file, lines, Charset.forName("UTF-8"));
-  }
+		while (!vorname.matches("[A-Z][a-z]*")) {
+			System.out
+					.println("Nur führende Großbuchstaben und folgende Kleinbuchstaben erlaubt!");
+			System.out.println("Vorname: ");
+			vorname = getScanner().next();
+		}
+		return vorname;
+	}
 
-  /**
-   * Load the persons from the given file. Each file should represent a given
-   * serialized person string.
-   * 
-   * @param filename
-   *          the file to load
-   * @throws IOException
-   */
-  public void loadPersons(String filename) throws IOException {
-    Path file = Paths.get(filename);
-    // TODO Exception Handling
-    List<String> allLines = Files.readAllLines(file, Charset.forName("UTF-8"));
-    for (String line : allLines) {
-      // Create Persons via the given serialized strings and add them to the
-      // `personen` array.
-      // TODO Exception Handling
-      personen.add(new Person(line));
-    }
-  }
+	private String erfasseName() {
+		String name = null;
+		name = getScanner().next();
 
-  /**
-   * @return the isRunning
-   */
-  public boolean isRunning() {
-    return isRunning;
-  }
+		// while (abcdf.length() < 3) {
+		while (!name.matches("[A-Z][a-z]*")) {
+			System.out
+					.println("Nur führende Großbuchstaben und folgende Kleinbuchstaben erlaubt!");
+			System.out.print("Name: ");
+			name = getScanner().next();
+		}
 
-  /**
-   * @param isRunning
-   *          the isRunning to set
-   */
-  public void setRunning(boolean isRunning) {
-    this.isRunning = isRunning;
-  }
+		return name;
+	}
 
-  /**
-   * @return the scanner
-   */
-  public Scanner getScanner() {
-    return scanner;
-  }
+	private String erfasseAnrede() {
+		String anrede = null;
+		anrede = getScanner().next();
+
+		// while (abcdf.length() < 3) {
+		while (!anrede.matches("Herr|Frau")) {
+			System.out.println("Bitte entweder Herr oder Frau angeben!");
+			System.out.print("Anrede: ");
+			anrede = getScanner().next();
+		}
+		return anrede;
+	}
+
+	private String erfasseStrasse() {
+		String strasse = null;
+		getScanner().nextLine();
+		strasse = getScanner().nextLine();
+
+		// while (abcdf.length() < 3) {
+		while (!strasse.matches("[A-Z][a-z]*\\s[0-9]*")) {
+			System.out
+					.println("Bitte Straßenname und Hausnr angeben bsp: Musterstrasse 5!");
+			System.out.print("Straße: ");
+			strasse = getScanner().nextLine();
+
+		}
+
+		return strasse;
+	}
+
+	private String erfassePLZ() {
+		String plz = null;
+		plz = getScanner().next();
+
+		// while (abcdf.length() < 3) {
+		while (!plz.matches("[0-9]{5}")) {
+			System.out.println("Zahlen von 0-9 und 5 stellen sind erlaubt!");
+			System.out.print("PLZ: ");
+			plz = getScanner().next();
+		}
+
+		return plz;
+	}
+
+	private String erfasseOrt() {
+		String ort = null;
+		getScanner().nextLine();
+		ort = getScanner().nextLine();
+
+		// while (abcdf.length() < 3) {
+		while (!ort.matches("[A-Z][a-z]*\\s[A-Za-z]*\\s[A-Za-z]*")) {
+			System.out
+					.println("Bitte den korrekten Ortsnamen angeben bsp: Frankfurt o. Frankfurt am Main");
+			System.out.print("Ort: ");
+			ort = getScanner().nextLine();
+		}
+
+		return ort;
+	}
+
+	private String erfasseTelefon() {
+		String telefon = null;
+		telefon = getScanner().next();
+
+		// while (abcdf.length() < 3) {
+		while (!telefon.matches("[0-9].{3,14}")) {
+			System.out
+					.println("Nur Zahlen von 0-9 und 3-14 stellen sind erlaubt!");
+			System.out.print("Telefon: ");
+			telefon = getScanner().next();
+		}
+
+		return telefon;
+	}
+
+	private String erfasseFax() {
+		String fax = null;
+		fax = getScanner().next();
+
+		// while (abcdf.length() < 3) {
+		while (!fax.matches("[0-9].{3,14}")) {
+			System.out
+					.println(" NurZahlen von 0-9 und 3-14 stellen sind erlaubt!");
+			System.out.print("Fax: ");
+			fax = getScanner().next();
+		}
+
+		return fax;
+	}
+
+	private String erfasseBemerkung() {
+		String bemerkung = null;
+		bemerkung = getScanner().next();
+
+		// while (abcdf.length() < 3) {
+		while (!bemerkung.matches("[^;]{4,300}")) {
+			System.out.println("Bitte Bemerkung korrekt eingeben !");
+			System.out.print("Bemerkung: ");
+			bemerkung = getScanner().next();
+		}
+
+		return bemerkung;
+	}
+
+	/**
+	 * Saves the in-memory `personen` to the given file.
+	 * 
+	 * @param filename
+	 *            the filename
+	 * @throws IOException
+	 */
+	public void savePersons(String filename) throws IOException {
+		List<String> lines = new ArrayList<>();
+
+		for (Person person : personen) {
+			// Create a serialized String of each person
+			lines.add(person.toSerializedString());
+		}
+
+		Path file = Paths.get(filename);
+		// TODO Zuerst file loeschen?
+		// TODO Exception Handling
+		// Write all serialized strings to the given file.
+		Files.write(file, lines, Charset.forName("UTF-8"));
+	}
+
+	/**
+	 * Load the persons from the given file. Each file should represent a given
+	 * serialized person string.
+	 * 
+	 * @param filename
+	 *            the file to load
+	 * @throws Exception
+	 * @throws IOException
+	 */
+	public void loadPersons(String filename) throws Exception {
+		Path file = Paths.get(filename);
+		// TODO Exception Handling
+		try {
+			List<String> allLines = Files.readAllLines(file,
+					Charset.forName("UTF-8"));
+			for (String line : allLines) {
+				// Create Persons via the given serialized strings and add them
+				// to
+				// the
+				// `personen` array.
+				// TODO Exception Handling
+				personen.add(new Person(line));
+			}
+		} catch (Exception ex) {
+			throw new Exception("Beim laden ist etwas schief gelaufen");
+		}
+
+	}
+
+	/**
+	 * @return the isRunning
+	 */
+	public boolean isRunning() {
+		return isRunning;
+	}
+
+	/**
+	 * @param isRunning
+	 *            the isRunning to set
+	 */
+	public void setRunning(boolean isRunning) {
+		this.isRunning = isRunning;
+	}
+
+	/**
+	 * @return the scanner
+	 */
+	public Scanner getScanner() {
+		return scanner;
+	}
+
+	private void sortiere() {
+		Collections.sort(personen);
+	}
 }
--- ProMod2/src/team/sheldon/Person.java
+++ ProMod2/src/team/sheldon/Person.java
@@ -3,54 +3,164 @@ package team.sheldon;
 /**
  * A Person ...
  */
-public class Person {
+public class Person implements Comparable<Person> {
 
-  /**
-   * Eine abfolge von speziellen Sonderzeichen, die verwendet werden, um die
-   * einzelnen Attribute einer Person in einem String zu Trennen. Bsp: { name:
-   * 'Bryant, vorname: 'Kobe', alter: 38 } => "Bryant%%%Kobe%%%38"
-   */
-  private static final String SEPARATOR = "%%%";
-  private String name;
-  private String vorname;
+	/**
+	 * Eine abfolge von speziellen Sonderzeichen, die verwendet werden, um die
+	 * einzelnen Attribute einer Person in einem String zu Trennen. Bsp: { name:
+	 * 'Bryant, vorname: 'Kobe', alter: 38 } => "Bryant%%%Kobe%%%38"
+	 */
+	public static final String SEPARATOR = "%%%";
+	private String name;
+	private String vorname;
+	private String anrede;
+	private String straße;
+	private String plz;
+	private String ort;
+	private String telefon;
+	private String fax;
+	private String bemerkung;
 
-  /**
-   * Constructor.
-   * 
-   * @param name
-   *          the name
-   * @param vorname
-   *          the vorname
-   */
-  public Person(String name, String vorname) {
-    this.name = name;
-    this.vorname = vorname;
-  }
+	/**
+	 * Constructor.
+	 * 
+	 * @param name
+	 *            the name
+	 * @param vorname
+	 *            the vorname
+	 */
+	public Person(String name, String vorname, String anrede, String straße,
+			String plz, String ort, String telefon, String fax, String bemerkung) {
+		this.setName(name);
+		this.setVorname(vorname);
+		this.setAnrede(anrede);
+		this.setStraße(straße);
+		this.setPLZ(plz);
+		this.setOrt(ort);
+		this.setTelefon(telefon);
+		this.setFax(fax);
+		this.setBemerkung(bemerkung);
+	}
 
-  /**
-   * Constructor to create a person via a serialized string. Each property has
-   * to be separated by the {@link #SEPARATOR}.
-   * 
-   * @param fromFileString the serialized string representing a person
-   */
-  public Person(String fromFileString) {
-    String[] strings = fromFileString.split(SEPARATOR);
-    // TODO Exception Handling
-    this.name = strings[0];
-    this.vorname = strings[1];
-  }
+	/**
+	 * Constructor to create a person via a serialized string. Each property has
+	 * to be separated by the {@link #SEPARATOR}.
+	 * 
+	 * @param fromFileString
+	 *            the serialized string representing a person
+	 */
+	public Person(String fromFileString) {
+		String[] strings = fromFileString.split(SEPARATOR);
+		// TODO Exception Handling
+		this.setName(strings[0]);
+		this.setVorname(strings[1]);
+		this.setAnrede(strings[2]);
+		this.setStraße(strings[3]);
+		this.setPLZ(strings[4]);
+		this.setOrt(strings[5]);
+		this.setTelefon(strings[6]);
+		this.setFax(strings[7]);
+		this.setBemerkung(strings[8]);
 
-  @Override
-  public String toString() {
-    return "[ " + "Name:" + name + "\n" + "Vorname: " + vorname + "]";
-  }
+	}
 
-  /**
-   * Serializes this person to a string.
-   * @return the serialized string
-   */
-  public String toSerializedString() {
-    // TODO all properties
-    return name + SEPARATOR + vorname;
-  }
+	@Override
+	public String toString() {
+		return "[ " + "Name:" + getName() + "\n" + "Vorname: " + getVorname()
+				+ "\n" + "Anrede: " + getAnrede() + "\n" + "Straße: "
+				+ getStraße() + "\n" + "PLZ:" + getPLZ() + "\n" + "Ort:"
+				+ getOrt() + "\n" + "Telefon:" + getTelefon() + "\n" + "Fax:"
+				+ getFax() + "\n" + "Bemerkung:" + getBemerkung() + "]";
+	}
+
+	/**
+	 * Serializes this person to a string.
+	 * 
+	 * @return the serialized string
+	 */
+	public String toSerializedString() {
+		// TODO all properties
+		return getName() + SEPARATOR + getVorname() + SEPARATOR + getAnrede()
+				+ SEPARATOR + getStraße() + SEPARATOR + getPLZ() + SEPARATOR
+				+ getOrt() + SEPARATOR + getTelefon() + SEPARATOR + getFax()
+				+ SEPARATOR + getBemerkung();
+	}
+
+	@Override
+	public int compareTo(Person p) {
+		// TODO Auto-generated method stub
+		return this.name.compareTo(p.getName());
+	}
+
+	public String getVorname() {
+		return vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAnrede() {
+		return anrede;
+	}
+
+	public void setAnrede(String anrede) {
+		this.anrede = anrede;
+	}
+
+	public String getStraße() {
+		return straße;
+	}
+
+	public void setStraße(String straße) {
+		this.straße = straße;
+	}
+
+	public String getPLZ() {
+		return plz;
+	}
+
+	public void setPLZ(String plz) {
+		this.plz = plz;
+	}
+
+	public String getOrt() {
+		return ort;
+	}
+
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
+
+	public String getTelefon() {
+		return telefon;
+	}
+
+	public void setTelefon(String telefon) {
+		this.telefon = telefon;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String getBemerkung() {
+		return bemerkung;
+	}
+
+	public void setBemerkung(String bemerkung) {
+		this.bemerkung = bemerkung;
+	}
 }
